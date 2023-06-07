8049c1b6c479aac2538ce5684059495e949d6d98
Andrew Peters <andrew.peters@operativa.dev>
2023-06-07 11:29:55 +0000

    Prettier

--- index.test.ts
+++ index.test.ts
@@ -1,16 +1,16 @@
 import { describe, expect, it } from "vitest";
-import postgres from 'postgres'
+import postgres from "postgres";
 
-const sql = postgres("postgres://postgres:postgres@db:5432/postgres")
+const sql = postgres("postgres://postgres:postgres@db:5432/postgres");
 
 describe("Test", () => {
     it("First", async () => {
-        const users =
-            await sql.unsafe("SELECT * FROM information_schema.tables LIMIT 5")
+        const users = await sql.unsafe(
+            "SELECT * FROM information_schema.tables LIMIT 5"
+        );
 
         for (const row of users) {
             console.log(row);
-
         }
-    })
-})
+    });
+});
