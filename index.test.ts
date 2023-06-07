c33544b4963946aa8b3f5dfee785175b601518db
Andrew Peters <andrew.peters@operativa.dev>
2023-06-07 10:56:43 +0000

    Setup PG

--- index.test.ts
+++ index.test.ts
@@ -1,7 +1,16 @@
 import { describe, expect, it } from "vitest";
+import postgres from 'postgres'
+
+const sql = postgres("postgres://postgres:postgres@db:5432/postgres")
 
 describe("Test", () => {
-    it("First", () => {
-        expect(2 + 2).toBe(4)
+    it("First", async () => {
+        const users =
+            await sql.unsafe("SELECT * FROM information_schema.tables LIMIT 5")
+
+        for (const row of users) {
+            console.log(row);
+
+        }
     })
 })
