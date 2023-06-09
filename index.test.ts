eeee65ed86748bcbf772177cdcf3024248b86c0b
Ben Janecke <ben.janecke@operativa.dev>
2023-06-09 09:16:59 +0000

    WIP

--- index.test.ts
+++ index.test.ts
@@ -1,67 +1,74 @@
-import {describe, expect, it} from "vitest";
+import { describe, expect, it } from "vitest";
 
-import {orm} from "./orm";
+import { orm } from "./orm";
+import { EntityBuilder, PropertyBuilder } from "./model";
 
 class Album {
-    constructor(readonly albumId: number, readonly title: string, readonly artistId: number) {
-    }
+  constructor(
+    readonly albumId: number,
+    readonly title: string,
+    readonly artistId: number
+  ) {}
 }
 
-const db = orm(ob => {
-    ob.model(mb => {
-        mb.entity(Album, e => {
-            e.property("albumId", p => p.column("AlbumId"));
-            e.property("title", p => p.column("Title"));
-            e.property("artistId", p => p.column("ArtistId"));
-        });
+const db = orm((ob) => {
+  ob.model((mb) => {
+    mb.entity(Album, (e) => {
+      e.properties({
+        albumId: (p) => p.column("AlbumId"),
+        title: { column: "Title" },
+        artistId: { column: "ArtistId", scale: 42 },
+      });
     });
+  });
 });
 
-
 describe("query", () => {
-    it("identity", async () => {
-        const q = db.query(Album);
+  it("identity", async () => {
+    const q = db.query(Album);
 
-        for await (const album of q()) {
-            //console.log(album);
-        }
-    });
+    for await (const album of q()) {
+      //console.log(album);
+    }
+  });
 
-    it("map template literal", async () => {
-        const q = db.query(Album, (albums, $title: string) => albums.filter(a => a.title == $title)
-                                                                    .map(a => `Title: ${a.title}!`));
+  it("map template literal", async () => {
+    const q = db.query(Album, (albums, $title: string) =>
+      albums.filter((a) => a.title == $title).map((a) => `Title: ${a.title}!`)
+    );
 
-        for await (const str of q("Miles Ahead")) {
-            //console.log(str);
-        }
-    });
+    for await (const str of q("Miles Ahead")) {
+      //console.log(str);
+    }
+  });
 
-    it("map object literal", async () => {
-        const q = db.query(Album, (albums, $title: string) => albums
-            .where(a => a.title == $title)
-            .select(a => ({msg: `Hi ${a.title}!`, sum: a.artistId * 2})));
+  it("map object literal", async () => {
+    const q = db.query(Album, (albums, $title: string) =>
+      albums
+        .where((a) => a.title == $title)
+        .select((a) => ({ msg: `Hi ${a.title}!`, sum: a.artistId * 2 }))
+    );
 
-        for await (const obj of q("Miles Ahead")) {
-            console.log(obj);
-        }
-    });
+    for await (const obj of q("Miles Ahead")) {
+      console.log(obj);
+    }
+  });
 
-    it("filter two args", async () => {
-        const q = db.query(Album,
-                           (albums, $title: string, $albumId: number) => albums.where(a => a.title == $title
-                                                                                           && a.albumId > $albumId));
+  it("filter two args", async () => {
+    const q = db.query(Album, (albums, $title: string, $albumId: number) =>
+      albums.where((a) => a.title == $title && a.albumId > $albumId)
+    );
 
-        for await (const album of q("Miles Ahead", 42)) {
-            //console.log(album);
-        }
-    });
+    for await (const album of q("Miles Ahead", 42)) {
+      //console.log(album);
+    }
+  });
 
-    it("closure throws", async () => {
-        const local = 42;
+  it("closure throws", async () => {
+    const local = 42;
 
-        expect(() => {
-            db.query(Album, albums => albums.where(a => a.albumId == local));
-        })
-            .toThrow("Unbound identifier 'local'");
-    });
+    expect(() => {
+      db.query(Album, (albums) => albums.where((a) => a.albumId == local));
+    }).toThrow("Unbound identifier 'local'");
+  });
 });
--- model.ts
+++ model.ts
@@ -1,144 +1,184 @@
-import {Map} from "immutable";
+import { Map } from "immutable";
 
 export class Model {
-    constructor(readonly entities: Map<new (...args: any[]) => any, EntityModel> = Map()) {
+  constructor(
+    readonly entities: Map<new (...args: any[]) => any, EntityModel> = Map()
+  ) {}
+
+  withEntity(key: new (...args: any[]) => any, model: EntityModel) {
+    const entities = this.entities.set(key, model);
+
+    if (entities === this.entities) {
+      return this;
     }
 
-    withEntity(key: new (...args: any[]) => any, model: EntityModel) {
-        const entities = this.entities.set(key, model);
-
-        if (entities === this.entities) {
-            return this;
-        }
-
-        return new Model(entities);
-    }
+    return new Model(entities);
+  }
 }
 
 export class ModelBuilder {
-    #model: Model;
+  #model: Model;
 
-    constructor(model: Model) {
-        this.#model = model;
+  constructor(model: Model) {
+    this.#model = model;
+  }
+
+  public get model(): Model {
+    return this.#model;
+  }
+
+  entity<T extends object>(
+    ctor: new (...args: any[]) => T,
+    builder: (eb: EntityBuilder<T>) => void
+  ): ModelBuilder {
+    let model = this.#model.entities.get(ctor);
+
+    if (!model) {
+      model = new EntityModel(ctor);
     }
 
-    public get model(): Model {
-        return this.#model;
-    }
+    const eb = new EntityBuilder<T>(model);
 
-    entity<T extends object>(ctor: new (...args: any[]) => T, builder: (eb: EntityBuilder<T>) => void): ModelBuilder {
-        let model = this.#model.entities.get(ctor);
+    builder(eb);
 
-        if (!model) {
-            model = new EntityModel(ctor);
-        }
+    this.#model = this.#model.withEntity(ctor, eb.model);
 
-        const eb = new EntityBuilder(model);
-
-        builder(eb);
-
-        this.#model = this.#model.withEntity(ctor, eb.model);
-
-        return this;
-    }
+    return this;
+  }
 }
 
 export class EntityModel {
-    constructor(
-        readonly ctor: new (...args: any[]) => any,
-        readonly properties: Map<string, PropertyModel> = Map(),
-        readonly table: string = ctor.name
-    ) {
-        if (properties.isEmpty()) {
-            const keys = Object.keys(new ctor());
-            this.properties = Map(keys.map(k => [k, new PropertyModel(k)]));
-        }
+  constructor(
+    readonly ctor: new (...args: any[]) => any,
+    readonly properties: Map<string, PropertyModel> = Map(),
+    readonly table: string = ctor.name
+  ) {
+    if (properties.isEmpty()) {
+      const keys = Object.keys(new ctor());
+      this.properties = Map(keys.map((k) => [k, new PropertyModel(k)]));
+    }
+  }
+
+  withProperty(key: string, model: PropertyModel): EntityModel {
+    const properties = this.properties.set(key, model);
+
+    if (properties === this.properties) {
+      return this;
     }
 
-    withProperty(key: string, model: PropertyModel): EntityModel {
-        const properties = this.properties.set(key, model);
+    return new EntityModel(this.ctor, properties, this.table);
+  }
 
-        if (properties === this.properties) {
-            return this;
-        }
-
-        return new EntityModel(this.ctor, properties, this.table);
+  withTable(table: string): EntityModel {
+    if (table === this.table) {
+      return this;
     }
 
-    withTable(table: string): EntityModel {
-        if (table === this.table) {
-            return this;
-        }
-
-        return new EntityModel(this.ctor, this.properties, table);
-    }
+    return new EntityModel(this.ctor, this.properties, table);
+  }
 }
 
-class EntityBuilder<T> {
-    #model: EntityModel;
+export class EntityBuilder<T> {
+  #model: EntityModel;
 
-    constructor(model: EntityModel) {
-        this.#model = model;
+  constructor(model: EntityModel) {
+    this.#model = model;
+  }
+
+  public get model(): EntityModel {
+    return this.#model;
+  }
+
+  properties<
+    S,
+    TProps extends {
+      [Key in keyof TProps & keyof T & string]:
+        | (<U>(pb: PropertyBuilder<U>) => PropertyBuilder<U>)
+        | ({
+            column?: string;
+          } & (T[Key] extends number ? { scale?: number } : {}));
     }
-
-    public get model(): EntityModel {
-        return this.#model;
-    }
-
-    property<S>(key: keyof T, builder: (pb: PropertyBuilder<S>) => void): EntityBuilder<T> {
-        if (typeof key !== "string") {
-            throw Error(`Property '${key.toString()}' must be a string.`);
-        }
-
-        const model = this.#model.properties.get(key);
-
+  >(props: TProps): EntityBuilder<T> {
+    (Object.keys(props) as (keyof T & keyof TProps & string)[]).forEach(
+      (prop) => {
+        const model = this.#model.properties.get(prop);
         if (!model) {
-            throw new Error(`Property '${key}' not found in model.`);
+          throw new Error(`Property '${prop}' not found in model.`);
+        }
+        const pb = new PropertyBuilder(model);
+        const config = props[prop as keyof TProps];
+
+        if (typeof config === "function") {
+          config(pb);
+        } else {
+          if (config.column) {
+            pb.column(config.column);
+          }
         }
 
-        const pb = new PropertyBuilder(model);
+        this.#model = this.#model.withProperty(prop, pb.model);
+      }
+    );
 
-        builder(pb);
+    return this;
+  }
 
-        this.#model = this.#model.withProperty(key, pb.model);
-
-        return this;
+  property<S>(
+    key: keyof T,
+    builder: (pb: PropertyBuilder<S>) => void
+  ): EntityBuilder<T> {
+    if (typeof key !== "string") {
+      throw Error(`Property '${key.toString()}' must be a string.`);
     }
 
-    table(name: string): EntityBuilder<T> {
-        this.#model = this.#model.withTable(name);
+    const model = this.#model.properties.get(key);
 
-        return this;
+    if (!model) {
+      throw new Error(`Property '${key}' not found in model.`);
     }
+
+    const pb = new PropertyBuilder(model);
+
+    builder(pb);
+
+    this.#model = this.#model.withProperty(key, pb.model);
+
+    return this;
+  }
+
+  table(name: string): EntityBuilder<T> {
+    this.#model = this.#model.withTable(name);
+
+    return this;
+  }
 }
 
 export class PropertyModel {
-    constructor(readonly name: string, readonly column: string = name) {
+  constructor(readonly name: string, readonly column: string = name) {}
+
+  withColumn(column: string): PropertyModel {
+    if (column === this.column) {
+      return this;
     }
 
-    withColumn(column: string): PropertyModel {
-        if (column === this.column) {
-            return this;
-        }
-
-        return new PropertyModel(column);
-    }
+    return new PropertyModel(column);
+  }
 }
 
-class PropertyBuilder<T> {
-    #model: PropertyModel;
+export class PropertyBuilder<T> {
+  #model: PropertyModel;
 
-    constructor(model: PropertyModel) {
-        this.#model = model;
-    }
+  constructor(model: PropertyModel) {
+    this.#model = model;
+  }
 
-    public get model(): PropertyModel {
-        return this.#model;
-    }
+  public get model(): PropertyModel {
+    return this.#model;
+  }
 
-    column(name: string): PropertyBuilder<T> {
-        this.#model = this.#model.withColumn(name);
+  column(name: string): PropertyBuilder<T> {
+    this.#model = this.#model.withColumn(name);
 
-        return this;
-    }
+    return this;
+  }
 }
